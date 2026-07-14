868e4ab676d38ca8b0bc93bc300e2480c21cbc3f
isu-nice <isu010614@naver.com>
2026-07-14 17:31:59 +0900

    feat: init albergue-service skeleton with health endpoing

---
+++ albergue-service/src/main/java/com/camino/albergue/AlbergueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.camino.albergue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbergueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbergueServiceApplication.class, args);
+	}
+
+}
